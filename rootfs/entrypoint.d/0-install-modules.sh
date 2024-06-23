2140659d21cacc813d4186f9cfa641f357a02a31
error418 <error418@users.noreply.github.com>
2024-06-23 21:16:47 +0200

    fix: use correct install arguments

--- rootfs/entrypoint.d/0-install-modules.sh
+++ rootfs/entrypoint.d/0-install-modules.sh
@@ -9,8 +9,7 @@ install() {
 if [[ -z "${PROSODY_EXTRA_MODULES}" ]]; then
   echo "no extra modules to install"
 else
-  for i in $PROSODY_EXTRA_MODULES
-  do
-    install $1
+  for i in $PROSODY_EXTRA_MODULES; do
+    install $i
   done
 fi
